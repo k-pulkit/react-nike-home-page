e6f777704608a4e56fd03778d91c13b828b7550e
k-pulkit <pulkit42041@gmail.com>
2024-02-09 18:23:14 -0500

    Marking the page structure + Nav bar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css'
+import { SpecialOffer, CustomerReviews, Footer, Hero, PopularProducts, Services, Subscribe, SuperQuality } from './sections';
+import Nav from './components/Nav';
 
-export default function App() {
+const App = () => {
   return(
-    <h1 className="text-3xl font-semibold underline">
-      Hello World
-    </h1>
+    <main className="relative">
+        <Nav />
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          <Hero />
+        </section>
+        <section className="padding">
+          <PopularProducts />
+        </section>
+        <section className="padding">
+          <SuperQuality />
+        </section>
+        <section className="padding">
+          <Services />
+        </section>
+        <section className="padding-x py-10">
+          <SpecialOffer />
+        </section>
+        <section className="padding bg-pale-blue">
+          <CustomerReviews />
+        </section>
+        <section className="padding-x sm:py-32 py-16 w-full">
+          <Subscribe />
+        </section>
+        <section className="bg-black padding-x padding-t pb-8">
+          <Footer />
+        </section>
+    </main>
   )
 }
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+
+export {SpecialOffer,CustomerReviews, Footer, Hero, PopularProducts, Services, Subscribe, SuperQuality};
