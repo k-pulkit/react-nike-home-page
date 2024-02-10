4873d5d3bca66a87a951217577a609db8d14604d
k-pulkit <pulkit42041@gmail.com>
2024-02-10 16:46:25 -0500

    Hero Section almost done

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({label, iconurl}) => {
+  return (
+    <button 
+    className="border border-coral-red font-montserrat text-white bg-coral-red text-lg leading-none
+               flex justify-center items-center gap-2 px-7 py-4 rounded-full
+               hover:border-red-800">
+        {label}
+        <img src={iconurl}
+        className='ml-2 w-5 h-5 rounded-full'
+        ></img>
+    </button>
+  )
+}
+
+export default Button
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import Button from '../components/Button';
+import { arrowRight } from '../assets/icons';
+import { statistics, shoes } from '../constants';
+import { bigShoe1 } from '../assets/images';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="max-container w-full min-h-screen flex xl:flex-row flex-col justify-center gap-10">
+      <div className="relative w-full xl:w-2/5 flex flex-col flex-0 justify-center items-start max-xl:padding-x pt-28">
+        <p class="text-xl text-coral-red font-montserrat">Our summer collection.</p>
+        <h1 className="mt-10 text-8xl text-black font-palanquin max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:text-nowrap pr-10 z-10 relative">The New Arrival</span>
+          <br />
+          <span className="text-coral-red mt-3 inline-block">Nike</span>
+          <span> Shoes</span>
+        </h1>
+        <p className="font-montserrat text-slate-gray mt-6 mb-14 leading-8 text-lg">Discover stylish Nike collection, innovation for your active life.</p>
+        {/* Button component */}
+        <Button label="Shop now" iconurl={arrowRight} />
+        {/* Stats section */}
+        <section className="w-full mt-20 flex flex-wrap justify-start items-start gap-16">
+          { statistics.map((stat, index) => (
+              <div key={index}>
+                <p className="text-4xl font-bold font-montserrat">{stat.value}</p>
+                <p className="text-xl font-palanquin text-slate-gray leading-8">{stat.label}</p>
+              </div>
+          )) }
+        </section>
+      </div>
+
+      <div className="relative max-xl:pt-40 flex flex-1 justify-center items-center w-full 
+                      xl:min-h-screen bg-primary bg-hero bg-cover bg-center">
+          <img
+           className='object-contain relative z-10'
+           src={bigShoe1}
+           width={610}
+           height={500}></img>
+
+           <div>
+              {shoes.map((shoe, index) => (
+                <div key={index}>
+                  <img
+                   src={shoe.thumbnail}
+                  ></img>
+                </div>
+              ))}
+           </div>
+      </div>
+    </section>
   )
 }
 
