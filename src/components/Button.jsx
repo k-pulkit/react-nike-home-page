d0edc33d0ecbae46c5e0c34c5c0a37d9d257a249
k-pulkit <pulkit42041@gmail.com>
2024-02-12 00:26:36 -0500

    Added the super quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,11 +7,14 @@ const Button = ({label, iconurl}) => {
                flex justify-center items-center gap-2 px-7 py-4 rounded-full
                hover:border-red-800">
         {label}
-        <img src={iconurl}
-        className='ml-2 w-5 h-5 rounded-full'
-        ></img>
+        {
+          iconurl &&
+          <img src={iconurl}
+            className='ml-2 w-5 h-5 rounded-full'
+          ></img>
+        }
     </button>
   )
 }
 
-export default Button
+export default Button;
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { shoe8 } from '../assets/images';
+import Button from '../components/Button';
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <div id="SuperQuality" className="flex flex-row justify-between max-container items-center">
+      <div className="flex flex-col"> 
+        <h1 className="text-4xl font-bold font-palanquin leading-tight">
+          We Provide You <span className='text-coral-red'> Super </span>
+          <br />
+          <span className='text-coral-red'>Quality </span>
+          Shoes
+        </h1>
+        <p className="mt-4 font-montserrat text-slate-gray text-lg md:max-w-lg leading-8">
+          <span>Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</span>
+          <br />
+          <span className="mt-4 inline-block">Our dedication to detail and excellence ensures your satisfaction</span>
+        </p>
+        <div className="mt-10">
+          <Button label="View Details"/>
+        </div>
+      </div>
+      <div className="flex flex-row justify-top items-start pr-10">
+        <img
+         className='object-contain'
+         src={shoe8}
+         width={580}
+         >
+        </img>
+      </div>
+    </div>
   )
 }
 
